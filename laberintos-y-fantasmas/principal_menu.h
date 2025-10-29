e45e7f09cc2bd9e0eeb5384f86127238d7a1c625
Joaquin <joaquinchinchurreta@gmail.com>
2025-10-28 21:27:26 -0300

    Arreglos 2

--- laberintos-y-fantasmas/principal_menu.h
+++ laberintos-y-fantasmas/principal_menu.h
@@ -1,23 +1,35 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 
-#define CANT_OPCIONES 3
 #define CANT_OPCIONES_PAUSA 2
 #define CANT_OPCIONES_CONFIRMACION 2
-#define TAM_PAL 50
 
+// Menu principal
 #define NUEVA_PARTIDA 0
 #define VER_RANKING 1
-#define SALIR 2
+#define DIFICULTAD 2
+#define SALIR 3
+#define CANT_OPCIONES_PRINCIPAL 4
 
+// Submenu de dificultad
+#define OPCION_FACIL 0
+#define OPCION_NORMAL 1
+#define OPCION_DIFICIL 2
+#define OPCION_PESADILLA 3
+#define CANT_OPCIONES_DIFICULTAD 4
+
+// Estado de Menu
 #define REANUDAR 3
 #define TERMINAR 0
 
-void limpiarConsola();
-void actualizarMenu(char matriz[][TAM_PAL], int cf, int *posAct, int posNueva);
-void mostrarMenu(char matriz[][TAM_PAL], int filas);
-int menu();
-int menuDePausa();
+#define TAM_PAL_OPCION 50
+
+void actualizarMenu(char matriz[][TAM_PAL_OPCION], int cf, int *posAct, int posNueva);
+void mostrarMenu(char matriz[][TAM_PAL_OPCION], int filas);
+void menuPrincipal();
 int menuDeConfirmacion();
+int menuDePausa();
+int subMenuDeDificultad();
+void menuDeDificultad();
 
 #endif // MENU_H_INCLUDED
